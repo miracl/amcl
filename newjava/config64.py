f9204fac038af17e45d653d3dfe8683e7a88cc55
mcarrickscott <mike.scott@certivox.com>
2017-03-01 09:12:41 +0000

    New Approach..

--- newjava/config64.py
+++ newjava/config64.py
@@ -1,7 +1,20 @@
 import os
 import sys
 
-javacmd=""
+deltext=""
+slashtext=""
+copytext=""
+if sys.platform.startswith("linux")  :
+	copytext="cp "
+	deltext="rm "
+	slashtext="/"
+if sys.platform.startswith("win") :
+	copytext="copy "
+	deltext="del "
+	slashtext="\\"
+
+chosen=[]
+cptr=0
 
 def replace(namefile,oldtext,newtext):
 	f = open(namefile,'r')
@@ -15,170 +28,97 @@ def replace(namefile,oldtext,newtext):
 	f.close()
 
 
-def rsaset(tb,tff,nb,base,ml) :
-	global javacmd
+def rsaset(tb,nb,base,ml) :
+	global deltext,slashtext,copytext
+	global cptr,chosen
 
-	copytext=""
-	if sys.platform.startswith("linux")  :
-		copytext="cp"
+	chosen.append(tb)
+	cptr=cptr+1
 
-	if sys.platform.startswith("win") :
-		copytext="copy"
+	fpath="amcl"+slashtext+tb+slashtext
+	os.system("mkdir amcl"+slashtext+tb)
 
-	fname="BIG_"+tb+".java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
-	os.system(copytext+" BIG_XXX_64.java "+fname)
-	replace(fname,"XXX",tb)
-	replace(fname,"@NB@",nb)
-	replace(fname,"@BASE@",base)
+	os.system(copytext+"BIG64.java "+fpath+"BIG.java")
+	os.system(copytext+"DBIG64.java "+fpath+"DBIG.java")
+	os.system(copytext+"FF64.java "+fpath+"FF.java")
+	os.system(copytext+"RSA.java "+fpath+"RSA.java")
+	os.system(copytext+"private_key.java "+fpath+"private_key.java")
+	os.system(copytext+"public_key.java "+fpath+"public_key.java")	
 
-
-
-	fname="DBIG_"+tb+".java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
-	os.system(copytext+" DBIG_XXX_64.java "+fname)
-	replace(fname,"XXX",tb)
-
-	fname="FF_"+tff+".java"
-	javacmd+=" "+fname
-	os.system(copytext+" FF_WWW_64.java "+fname)
-	replace(fname,"XXX",tb)
-	replace(fname,"WWW",tff)
-	replace(fname,"@ML@",ml);
+	replace(fpath+"BIG.java","XXX",tb)
+	replace(fpath+"DBIG.java","XXX",tb)
+	replace(fpath+"FF.java","XXX",tb)
+	replace(fpath+"RSA.java","XXX",tb)
+	replace(fpath+"private_key.java","XXX",tb)
+	replace(fpath+"public_key.java","XXX",tb)
 
 
-	fname="RSA_"+tff+".java"
-	javacmd+=" "+fname
+	replace(fpath+"BIG.java","@NB@",nb)
+	replace(fpath+"BIG.java","@BASE@",base)
 
-	os.system(copytext+" RSA_WWW.java "+fname)
+	replace(fpath+"FF.java","@ML@",ml);
 
-	replace(fname,"WWW",tff)
-	replace(fname,"XXX",tb)
-
-	fname="rsa_private_key_"+tff+".java"
-	javacmd+=" "+fname
-
-	os.system(copytext+" rsa_private_key_WWW.java "+fname)
-	replace(fname,"WWW",tff)
+	os.system("javac "+fpath+"*.java")
 
 
-	fname="rsa_public_key_"+tff+".java"
-	javacmd+=" "+fname
+def curveset(tc,nb,base,nbt,m8,mt,ct,pf) :
+	global deltext,slashtext,copytext
+	global cptr,chosen
 
-	os.system(copytext+" rsa_public_key_WWW.java "+fname)
-	replace(fname,"WWW",tff)
+	chosen.append(tc)
+	cptr=cptr+1
 
+	fpath="amcl"+slashtext+tc+slashtext
+	os.system("mkdir amcl"+slashtext+tc)
 
-def curveset(tb,tf,tc,nb,base,nbt,m8,mt,ct,pf) :
-	global javacmd
+	os.system(copytext+"BIG64.java "+fpath+"BIG.java")
+	os.system(copytext+"DBIG64.java "+fpath+"DBIG.java")
+	os.system(copytext+"FP64.java "+fpath+"FP.java")
+	os.system(copytext+"ECP.java "+fpath+"ECP.java")
+	os.system(copytext+"ECDH.java "+fpath+"ECDH.java")
+	os.system(copytext+"ROM_"+tc+"_64.java "+fpath+"ROM.java")
 
-	copytext=""
-	if sys.platform.startswith("linux")  :
-		copytext="cp"
+	
+	replace(fpath+"BIG.java","XXX",tc)
+	replace(fpath+"DBIG.java","XXX",tc)
+	replace(fpath+"FP.java","XXX",tc)
+	replace(fpath+"ECP.java","XXX",tc)
+	replace(fpath+"ECDH.java","XXX",tc)
 
-	if sys.platform.startswith("win") :
-		copytext="copy"
+	replace(fpath+"BIG.java","@NB@",nb)
+	replace(fpath+"BIG.java","@BASE@",base)
 
-	fname="BIG_"+tb+".java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
-	os.system(copytext+" BIG_XXX_64.java "+fname)
-	replace(fname,"XXX",tb)
-	replace(fname,"@NB@",nb)
-	replace(fname,"@BASE@",base)
+	replace(fpath+"FP.java","@NBT@",nbt)
+	replace(fpath+"FP.java","@M8@",m8)
+	replace(fpath+"FP.java","@MT@",mt)
 
-	fname="DBIG_"+tb+".java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
-	os.system(copytext+" DBIG_XXX_64.java "+fname)
-	replace(fname,"XXX",tb)
+	replace(fpath+"ECP.java","@CT@",ct)
+	replace(fpath+"ECP.java","@PF@",pf)
 
-	fname="FP_"+tf+".java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
-	os.system(copytext+" FP_YYY_64.java "+fname)
-	replace(fname,"XXX",tb)
-	replace(fname,"YYY",tf)
-	replace(fname,"@NBT@",nbt)
-	replace(fname,"@M8@",m8)
-	replace(fname,"@MT@",mt)
-
-	fname="ROM_"+tf+".java"
-	javacmd+=" "+fname
-	os.system(copytext+" ROM_"+tf+"_64.java "+fname);
-
-	fname="ECP_"+tc+".java"	
-	javacmd+=" "+fname
-	os.system(copytext+" ECP_ZZZ.java "+fname)
-	replace(fname,"XXX",tb)
-	replace(fname,"YYY",tf)
-	replace(fname,"ZZZ",tc)
-	replace(fname,"@CT@",ct)
-	replace(fname,"@PF@",pf)
-
-	fname="ROM_"+tc+".java"
-	javacmd+=" "+fname
-	os.system(copytext+" ROM_"+tc+"_64.java "+fname);
-
-	fname="ECDH_"+tc+".java"
-	javacmd+=" "+fname
-	os.system(copytext+" ECDH_ZZZ.java "+fname)
-
-	replace(fname,"ZZZ",tc)
-	replace(fname,"YYY",tf)
-	replace(fname,"XXX",tb)
-
-	fname="ECDH_SUP.java"
-	if fname not in javacmd:
-		javacmd+=" "+fname
 
 	if pf != "NOT" :
-		fname="FP2_"+tf+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" FP2_YYY.java "+fname)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
+		os.system(copytext+"ECP2.java "+fpath+"ECP2.java")
+		os.system(copytext+"FP2.java "+fpath+"FP2.java")
+		os.system(copytext+"FP4.java "+fpath+"FP4.java")
+		os.system(copytext+"FP12.java "+fpath+"FP12.java")
+		os.system(copytext+"PAIR.java "+fpath+"PAIR.java")
+		os.system(copytext+"MPIN.java "+fpath+"MPIN.java")
 
-		fname="FP4_"+tf+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" FP4_YYY.java "+fname)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
-
-		fname="FP12_"+tf+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" FP12_YYY.java "+fname)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
+		replace(fpath+"FP2.java","XXX",tc)
+		replace(fpath+"FP4.java","XXX",tc)
+		replace(fpath+"FP12.java","XXX",tc)
+		replace(fpath+"ECP2.java","XXX",tc)
+		replace(fpath+"PAIR.java","XXX",tc)
+		replace(fpath+"MPIN.java","XXX",tc)
+	
+	os.system("javac "+fpath+"*.java")
 
 
-		fname="ECP2_"+tc+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" ECP2_ZZZ.java "+fname)
-		replace(fname,"ZZZ",tc)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
-
-		fname="PAIR_"+tc+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" PAIR_ZZZ.java "+fname)
-		replace(fname,"ZZZ",tc)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
-
-		fname="MPIN_"+tc+".java"
-		javacmd+=" "+fname
-		os.system(copytext+" MPIN_ZZZ.java "+fname)
-		replace(fname,"ZZZ",tc)
-		replace(fname,"YYY",tf)
-		replace(fname,"XXX",tb)
-
-		fname="MPIN_SUP.java"
-		if fname not in javacmd:
-			javacmd+=" "+fname
-
+os.system("mkdir amcl")
+os.system(copytext+ "HASH*.java amcl"+slashtext+".")
+os.system(copytext+ "RAND.java amcl"+slashtext+".")
+os.system(copytext+ "AES.java amcl"+slashtext+".")
+os.system(copytext+ "GCM.java amcl"+slashtext+".")
 
 print("Elliptic Curves")
 print("1. ED25519")
@@ -239,94 +179,93 @@ while ptr<max:
 	selection.append(x)
 	ptr=ptr+1
 
-# curveset(big,field,curve,big_length_bytes,16_bit_bits_in_base,32_bit_bits_in_base,64_bit_bits_in_base,modulus_bits,modulus_mod_8,modulus_type,curve_type,pairing_friendly)
-# for each curve give distinct names for big, field and curve. 
-# Typically "big" is the size in bits, always a multiple of 8, "field" describes the modulus, and "curve" is the common name for the elliptic curve   
-# big_length_bytes is "big" divided by 8
-# Next give the number base used for 16/32/64 bit architectures, as n where the base is 2^n (note that these must be fixed for the same "big" name, if is ever re-used for another curve)
-# modulus_bits is the bit length of the modulus, typically the same or slightly smaller than "big"
+# curveset(curve,big_length_bytes,bits_in_base,modulus_bits,modulus_mod_8,modulus_type,curve_type,pairing_friendly)
+# where "curve" is the common name for the elliptic curve   
+# big_length_bytes is the modulus size rounded up to a number of bytes
+# bits_in_base gives the number base used for 64 bit architectures, as n where the base is 2^n
+# modulus_bits is the actual bit length of the modulus.
 # modulus_mod_8 is the remainder when the modulus is divided by 8
 # modulus_type is NOT_SPECIAL, or PSEUDO_MERSENNE, or MONTGOMERY_Friendly, or GENERALISED_MERSENNE (supported for GOLDILOCKS only)
 # curve_type is WEIERSTRASS, EDWARDS or MONTGOMERY
 # pairing_friendly is BN, BLS or NOT (if not pairing friendly
 
 	if x==1:
-		curveset("256","25519","ED25519","32","56","255","5","PSEUDO_MERSENNE","EDWARDS","NOT")
+		curveset("ED25519","32","56","255","5","PSEUDO_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==2:
-		curveset("256","25519","C25519","32","56","255","5","PSEUDO_MERSENNE","MONTGOMERY","NOT")
+		curveset("C25519","32","56","255","5","PSEUDO_MERSENNE","MONTGOMERY","NOT")
 		curve_selected=True
 	if x==3:
-		curveset("256","GM256","NIST256","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
+		curveset("NIST256","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==4:
-		curveset("256","B256","BRAINPOOL","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
+		curveset("BRAINPOOL","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==5:
-		curveset("256","A256","ANNSI","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
+		curveset("ANNSI","32","56","256","7","NOT_SPECIAL","WEIERSTRASS","NOT")
 		curve_selected=True
 
 	if x==6:
-		curveset("336","336PM","HIFIVE","42","60","336","5","PSEUDO_MERSENNE","EDWARDS","NOT")
+		curveset("HIFIVE","42","60","336","5","PSEUDO_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==7:
-		curveset("448","448GM","GOLDILOCKS","56","60","448","7","GENERALISED_MERSENNE","EDWARDS","NOT")
+		curveset("GOLDILOCKS","56","60","448","7","GENERALISED_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==8:
-		curveset("384","GM384","NIST384","48","56","384","7","NOT_SPECIAL","WEIERSTRASS","NOT")
+		curveset("NIST384","48","56","384","7","NOT_SPECIAL","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==9:
-		curveset("416","414PM","C41417","52","60","414","7","PSEUDO_MERSENNE","EDWARDS","NOT")
+		curveset("C41417","52","60","414","7","PSEUDO_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==10:
-		curveset("528","521M","NIST521","66","60","521","7","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
+		curveset("NIST521","66","60","521","7","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
 		curve_selected=True
 
 	if x==11:
-		curveset("256","254MF","MF254W","32","56","254","7","MONTGOMERY_FRIENDLY","WEIERSTRASS","NOT")
+		curveset("MF254W","32","56","254","7","MONTGOMERY_FRIENDLY","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==12:
-		curveset("256","254MF","MF254E","32","56","254","7","MONTGOMERY_FRIENDLY","EDWARDS","NOT")
+		curveset("MF254E","32","56","254","7","MONTGOMERY_FRIENDLY","EDWARDS","NOT")
 		curve_selected=True
 	if x==13:
-		curveset("256","254MF","MF254M","32","56","254","7","MONTGOMERY_FRIENDLY","MONTGOMERY","NOT")
+		curveset("MF254M","32","56","254","7","MONTGOMERY_FRIENDLY","MONTGOMERY","NOT")
 		curve_selected=True
 	if x==14:
-		curveset("256","256MF","MF256W","32","56","256","7","MONTGOMERY_FRIENDLY","WEIERSTRASS","NOT")
+		curveset("MF256W","32","56","256","7","MONTGOMERY_FRIENDLY","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==15:
-		curveset("256","256MF","MF256E","32","56","256","7","MONTGOMERY_FRIENDLY","EDWARDS","NOT")
+		curveset("MF256E","32","56","256","7","MONTGOMERY_FRIENDLY","EDWARDS","NOT")
 		curve_selected=True
 	if x==16:
-		curveset("256","256MF","MF256M","32","56","256","7","MONTGOMERY_FRIENDLY","MONTGOMERY","NOT")
+		curveset("MF256M","32","56","256","7","MONTGOMERY_FRIENDLY","MONTGOMERY","NOT")
 		curve_selected=True
 	if x==17:
-		curveset("256","255MS","MS255W","32","56","255","3","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
+		curveset("MS255W","32","56","255","3","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==18:
-		curveset("256","255MS","MS255E","32","56","255","3","PSEUDO_MERSENNE","EDWARDS","NOT")
+		curveset("MS255E","32","56","255","3","PSEUDO_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==19:
-		curveset("256","255MS","MS255M","32","56","255","3","PSEUDO_MERSENNE","MONTGOMERY","NOT")
+		curveset("MS255M","32","56","255","3","PSEUDO_MERSENNE","MONTGOMERY","NOT")
 		curve_selected=True
 	if x==20:
-		curveset("256","256MS","MS256W","32","56","256","3","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
+		curveset("MS256W","32","56","256","3","PSEUDO_MERSENNE","WEIERSTRASS","NOT")
 		curve_selected=True
 	if x==21:
-		curveset("256","256MS","MS256E","32","56","256","3","PSEUDO_MERSENNE","EDWARDS","NOT")
+		curveset("MS256E","32","56","256","3","PSEUDO_MERSENNE","EDWARDS","NOT")
 		curve_selected=True
 	if x==22:
-		curveset("256","256MS","MS256M","32","56","256","3","PSEUDO_MERSENNE","MONTGOMERY","NOT")
+		curveset("MS256M","32","56","256","3","PSEUDO_MERSENNE","MONTGOMERY","NOT")
 		curve_selected=True
 
 	if x==23:
-		curveset("256","254BN","BN254","32","56","254","3","NOT_SPECIAL","WEIERSTRASS","BN")
+		curveset("BN254","32","56","254","3","NOT_SPECIAL","WEIERSTRASS","BN")
 		pfcurve_selected=True
 	if x==24:
-		curveset("256","254BNCX","BN254CX","32","56","254","3","NOT_SPECIAL","WEIERSTRASS","BN")
+		curveset("BN254CX","32","56","254","3","NOT_SPECIAL","WEIERSTRASS","BN")
 		pfcurve_selected=True
 	if x==25:
-		curveset("384","383BLS","BLS383","48","56","383","3","NOT_SPECIAL","WEIERSTRASS","BLS")
+		curveset("BLS383","48","56","383","3","NOT_SPECIAL","WEIERSTRASS","BLS")
 		pfcurve_selected=True
 
 # rsaset(big,ring,big_length_bytes,16_bit_bits_in_base,32_bit_bits_in_base,64_bit_bits_in_base,multiplier)
@@ -341,54 +280,48 @@ while ptr<max:
 	if x==26:
 		#256 is slower but may allow reuse of 256-bit BIGs used for elliptic curve
 		#512 is faster.. but best is 1024
-		rsaset("1024","2048","128","58","2")
-		#rsaset("512","2048","64","60","4")
-		#rsaset("256","2048","32","56","8")
+		rsaset("RSA2048","128","58","2")
+		#rsaset("RSA2048","64","60","4")
+		#rsaset("RSA2048","32","56","8")
 		rsa_selected=True
 	if x==27:
-		rsaset("384","3072","48","56","8")
+		rsaset("RSA3072","48","56","8")
 		rsa_selected=True
 	if x==28:
-		#rsaset("256","4096","32","56","16")
-		rsaset("512","4096","64","60","8")
+		#rsaset("RSA4096","32","29","16")
+		rsaset("RSA4096","64","60","8")
 		rsa_selected=True
 
-deltext=""
-slashtext=""
-if sys.platform.startswith("linux")  :
-	deltext="rm"
-	slashtext="/"
 
-if sys.platform.startswith("win") :
-	deltext="del"
-	slashtext="\\"
+os.system(deltext+" HASH*.java")
+os.system(deltext+" AES.java")
+os.system(deltext+" RAND.java")
+os.system(deltext+" GCM.java")
 
-
-os.system(deltext+" BIG_XXX*.*")
-os.system(deltext+" DBIG_XXX*.*")
-os.system(deltext+" FP_YYY*.*")
-os.system(deltext+" ECP_ZZZ.*")
-os.system(deltext+" ECDH_ZZZ.*")
-os.system(deltext+" FF_WWW*.*")
-os.system(deltext+" RSA_WWW.*")
-os.system(deltext+" rsa_public_key_WWW.*");
-os.system(deltext+" rsa_private_key_WWW.*");
-os.system(deltext+" FP2_YYY.*")
-os.system(deltext+" FP4_YYY.*")
-os.system(deltext+" FP12_YYY.*")
-os.system(deltext+" ECP2_ZZZ.*")
-os.system(deltext+" PAIR_ZZZ.*")
-os.system(deltext+" MPIN_ZZZ.*")
+os.system(deltext+" BIG*.java")
+os.system(deltext+" DBIG*.java")
+os.system(deltext+" FP*.java")
+os.system(deltext+" ECP.java")
+os.system(deltext+" ECDH.java")
+os.system(deltext+" FF*.java")
+os.system(deltext+" RSA.java")
+os.system(deltext+" public_key.java")
+os.system(deltext+" private_key.java")
+os.system(deltext+" ECP2.java")
+os.system(deltext+" PAIR.java")
+os.system(deltext+" MPIN.java")
+os.system(deltext+" ROM*.java")
 
 # create library
 
-#print("javac "+javacmd+"\n")
-os.system("javac -d . "+javacmd+" AES.java RAND.java GCM.java HASH256.java HASH384.java HASH512.java")
-os.system("jar cf amcl.jar amcl/*.class")
+os.system("jar cf amcl.jar amcl")
+
+for i in range(0,cptr):
+	os.system(deltext+" amcl"+slashtext+chosen[i]+slashtext+"*.java")
+	os.system(deltext+" amcl"+slashtext+chosen[i]+slashtext+"*.class")
+	os.system("rmdir amcl"+slashtext+chosen[i])
+
+os.system(deltext+" amcl"+slashtext+"*.java")
 os.system(deltext+" amcl"+slashtext+"*.class")
 os.system("rmdir amcl")
-
-#print("Your section was ");	
-#for i in range(0,ptr):
-#	print (selection[i])
 
--- newjava/readme.txt
+++ newjava/readme.txt
@@ -17,19 +17,15 @@ This obviously increases code size, roughly it will now be a multiple of
 the number of curves supported. But otherwise the code itself does not 
 change.
 
-To this end the BIG type is renamed to BIG_XXX, where XXX can be changed to 
-describe the size of the BIG variable. Similarily the FP type is renamed
-FP_YYY, where YYY reflects the modulus used. Also the ECP type is renamed
-ECP_ZZZ, where ZZZ describes the actual curve. 
+To this end each supported primitive is implemented inside of its own 
+java package. 
 
-So for example to support both ED25519 and the NIST P256 curve, we would 
-need to create BIG_256, FP_25519, ECP_ED25519 and FP_GM256, ECP_NIST256. 
-Note that both curves could be built on top of BIG_256, as both require 
-support for 256-bit numbers.
+So for example to support both ED25519 and the NIST P256 curves, one
+could import into a particular module either "amcl.ED25519" or "amcl.NIST256"
 
 Separate ROM files provide the constants required for each curve. Some
-files (BIG_XXX.java, FP_YYY.java and ECP_ZZZ.java) also specify 
-certain constants that must be set for the particular curve.
+files (BIG.java, FP.java, ECP.java) also specify certain constants 
+that must be set for the particular curve.
 
 --------------------------------------
 
@@ -47,8 +43,8 @@ or
 
 python3 config64.py
 
-Then select options 1, 3, 7, 23 and 26 (these are fixed for the example 
-program). Select 0 then to exit.
+Then select options 1, 23 and 26 (these are fixed for the example 
+programs provided). Select 0 to exit.
 
 Then compile and run the test program by executing on windows
 
