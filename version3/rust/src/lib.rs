575eac50c424c892f400719543dacd0f69549c8a
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-11-29 12:17:56 +0300

    Example without macroses
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- version3/rust/src/lib.rs
+++ version3/rust/src/lib.rs
@@ -7,46 +7,68 @@ pub mod rand;
 pub mod sha3;
 pub mod nhs;
 
-macro_rules! rom_mod {
-    ($path: expr) => {
-        #[path = $path]
-        mod rom;
-    };
+#[cfg(feature = "rsa2048")]
+#[path = "./"]
+pub mod rsa2048 {
+    #[cfg(target_pointer_width = "32")]
+    #[path = "arch/arch32.rs"]
+    mod arch;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "arch/arch64.rs"]
+    mod arch;
+
+    #[cfg(target_pointer_width = "32")]
+    #[path = "roms/rom_rsa2048_32.rs"]
+    mod rom;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "roms/rom_rsa2048_64.rs"]
+    mod rom;
+    pub mod big;
+    pub mod dbig;
+    pub mod ff;
+    pub mod rsa;
 }
 
-macro_rules! arch {
-    () => {
-        #[cfg(target_pointer_width = "32")]
-        #[path = "arch/arch32.rs"]
-        mod arch;
-        #[cfg(target_pointer_width = "64")]
-        #[path = "arch/arch64.rs"]
-        mod arch;
-    };
+#[cfg(feature = "rsa3072")]
+#[path = "./"]
+pub mod rsa3072 {
+    #[cfg(target_pointer_width = "32")]
+    #[path = "arch/arch32.rs"]
+    mod arch;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "arch/arch64.rs"]
+    mod arch;
+
+    #[cfg(target_pointer_width = "32")]
+    #[path = "roms/rom_rsa3072_32.rs"]
+    mod rom;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "roms/rom_rsa3072_64.rs"]
+    mod rom;
+    pub mod big;
+    pub mod dbig;
+    pub mod ff;
+    pub mod rsa;
 }
 
-macro_rules! curve {
-    () => {};
-}
+#[cfg(feature = "rsa4096")]
+#[path = "./"]
+pub mod rsa4096 {
+    #[cfg(target_pointer_width = "32")]
+    #[path = "arch/arch32.rs"]
+    mod arch;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "arch/arch64.rs"]
+    mod arch;
 
-macro_rules! rsa {
-    ($name: tt, $feature: expr, $path32: expr, $path64: expr) => {
-        #[cfg(feature = $feature)]
-        #[path = "./"]
-        pub mod $name {
-            arch!();
-            #[cfg(target_pointer_width = "32")]
-            rom_mod!($path32);
-            #[cfg(target_pointer_width = "64")]
-            rom_mod!($path64);
-            pub mod big;
-            pub mod dbig;
-            pub mod ff;
-            pub mod rsa;
-        }
-    };
+    #[cfg(target_pointer_width = "32")]
+    #[path = "roms/rom_rsa4096_32.rs"]
+    mod rom;
+    #[cfg(target_pointer_width = "64")]
+    #[path = "roms/rom_rsa4096_64.rs"]
+    mod rom;
+    pub mod big;
+    pub mod dbig;
+    pub mod ff;
+    pub mod rsa;
 }
-
-rsa!(rsa2048, "rsa2048", "roms/rom_rsa2048_32.rs", "roms/rom_rsa2048_64.rs");
-rsa!(rsa3072, "rsa3072", "roms/rom_rsa3072_32.rs", "roms/rom_rsa3072_64.rs");
-rsa!(rsa2048, "rsa3072", "roms/rom_rsa4096_32.rs", "roms/rom_rsa4096_64.rs");
