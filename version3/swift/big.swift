miracl/amcl

commit 48839824dc2833ef73607ae6fc2ff3642796ad0e

Fix swift bugs

version3/swift/big.swift
@@ -105,7 +105,7 @@ public struct BIG{
     static func muladd(_ a: Chunk,_ b: Chunk,_ c: Chunk,_ r: Chunk) -> (Chunk,Chunk)
     {
         let (tp,bt)=a.multipliedFullWidth(by: b)
-        var bot = Chunk(bt)&CONFIG_BIG.BMASK
+        var bot = Chunk(bt&(UInt64(CONFIG_BIG.BMASK))
         var top = (tp << Chunk(64-CONFIG_BIG.BASEBITS)) | Chunk(bt >> CONFIG_BIG.BASEBITS)
 
         bot+=c; bot+=r

version3/swift/bls.swift
@@ -38,7 +38,7 @@ public struct BLS
     
 /* hash a message to an ECP point, using SHA3 */
 
-    private static func bls_hashit(_ m: String) -> ECP
+    static public func bls_hashit(_ m: String) -> ECP
     {
         var sh=SHA3(SHA3.SHAKE256)
         var hm=[UInt8](repeating: 0,count: BFS)  

version3/swift/bls192.swift
@@ -38,7 +38,7 @@ public struct BLS192
     
 /* hash a message to an ECP point, using SHA3 */
 
-    private static func bls_hashit(_ m: String) -> ECP
+    static public func bls_hashit(_ m: String) -> ECP
     {
         var sh=SHA3(SHA3.SHAKE256)
         var hm=[UInt8](repeating: 0,count: BFS)  

version3/swift/bls256.swift
@@ -38,7 +38,7 @@ public struct BLS256
     
 /* hash a message to an ECP point, using SHA3 */
 
-    private static func bls_hashit(_ m: String) -> ECP
+    static public func bls_hashit(_ m: String) -> ECP
     {
         var sh=SHA3(SHA3.SHAKE256)
         var hm=[UInt8](repeating: 0,count: BFS)  

commit f0fc6a23f7c6b791892410dd161ce9992a35ece1

Fix syntax error in swift code
This broke in 48839824dc2833ef73607ae6fc2ff3642796ad0e

version3/swift/big.swift
@@ -105,7 +105,7 @@ public struct BIG{
     static func muladd(_ a: Chunk,_ b: Chunk,_ c: Chunk,_ r: Chunk) -> (Chunk,Chunk)
     {
         let (tp,bt)=a.multipliedFullWidth(by: b)
-        var bot = Chunk(bt&(UInt64(CONFIG_BIG.BMASK))
+        var bot = Chunk(bt&(UInt64(CONFIG_BIG.BMASK)))
         var top = (tp << Chunk(64-CONFIG_BIG.BASEBITS)) | Chunk(bt >> CONFIG_BIG.BASEBITS)
 
         bot+=c; bot+=r